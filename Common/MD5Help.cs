bcea9366c6db1afec125476936fd4c3171ce726b
shiguang3 <1247499894@qq.com>
2019-03-25 10:34:12 +0800

    MD5加密

---
+++ Common/MD5Help.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class MD5Help
+    {
+        public static string GetMd5String(string str)
+        {
+            MD5 md5 = MD5.Create();
+            str += "lcm";
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(str);
+            byte[] md5Buffer = md5.ComputeHash(buffer);
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in md5Buffer)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
